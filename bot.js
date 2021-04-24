f128fd955b4c15656b88a6751b3297a9bc244b5a
Siwo951 <56748372+Siwo951@users.noreply.github.com>
2021-04-24 17:17:48 +0900

    微細な手直し

--- bot.js
+++ bot.js
@@ -2,26 +2,25 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const client = new Discord.Client();
-const command = require("./config.js")["command"];
-const token = require("./config.js")["token"];
+const config = require("./config");
 
 // ------------------------- //
 
 if (process.versions.node < 12) {
   console.log("--------------------");
-  console.log("Node.JSのバージョンがv12以降ではありません");
-  console.log("Node.JSのバージョンをv12以降に更新して下さい");
+  console.log("Node.jsのバージョンがv12以降ではありません");
+  console.log("Node.jsのバージョンをv12以降に更新して下さい");
   console.log("--------------------");
   process.exit(0);
 }
-if (command == "") {
+if (config.command == "") {
   console.log("--------------------");
   console.log("config.jsの command が空欄になっています");
   console.log("記入して再度実行して下さい");
   console.log("--------------------");
   process.exit(0);
 }
-if (token == "") {
+if (config.token == "") {
   console.log("--------------------");
   console.log("config.jsの token が空欄になっています");
   console.log("記入して再度実行して下さい");
@@ -35,71 +34,81 @@ client.on("ready", () => {
   console.log("--------------------");
   console.log("起動しました");
   console.log("--------------------");
-  client.user.setActivity(`${command} [追跡番号]`);
+  client.user.setActivity(`${config.command} [追跡番号]`);
 });
 
 // ------------------------- //
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if (message.content.startsWith(command)) {
-    var trackCode = message.content.replace(`${command}`,"").replace(/\s+/g,"");
-    fetch(`https://trackings.post.japanpost.jp/services/srv/search/?requestNo1=${trackCode}&search=%E8%BF%BD%E8%B7%A1%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%88`)
-    .then(res => res.text())
-    .then(body => {
-      var $ = cheerio.load(body);
-      if ($(".txt_l > font:nth-child(1)").text() == "") {
-        var japanpostInfo = [];
-        var japanpostDate = [];
-        var japanpostSet = [];
-        var japanPostPlaceName = [];
-        var japanpostType;
-        var japanpostAuthor = `日本郵便`;
-        $("table.tableType01:nth-child(5) > tbody > tr > td:nth-child(2)").each((i, elem) => {japanpostInfo[i] = $(elem).text()});
-        $("table.tableType01:nth-child(5) > tbody > tr > td.w_120:nth-child(1)").each((i, elem) => {japanpostDate[i] = $(elem).text()});
-        if ($(".ttl_line").text().indexOf("ゆうパック") > -1) {
-          japanpostType = $("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text();
-          for (let i = 0; i < japanpostInfo.length; i++) japanpostSet.push({name: `${japanpostDate[i].slice(5)}`, value: `${japanpostInfo[i]}`});
-        }
-        else if ($(".ttl_line").text().indexOf("国際") > -1) {
-          japanpostAuthor = `日本郵便 / 国際郵便`;
-          japanpostType = `${$("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text()}\n(海外での履歴は現地時間)`;
-          $("table.tableType01:nth-child(5) > tbody:nth-child(1) > tr > td:nth-child(5)").each((i, elem) => {japanPostPlaceName[i] = $(elem).text()});
-          for (let i = 0; i < japanpostInfo.length; i++) {
-            if (japanPostPlaceName[i].replace(/\s+/g,"").replace(/[A-Z]/g,"") == "") {
-              japanpostSet.push({name: `${japanpostDate[i].slice(5)} (${japanPostPlaceName[i].replace(/\s+/g,"")})`, value: `${japanpostInfo[i]}`});
+  if (message.content.startsWith(config.command)) {
+    message.channel.send({
+      embed: {
+        author: {
+          name: `Loading...`
+        },
+        color: `0x292929`,
+        title: `荷物の状況: 読み込み中...`,
+        timestamp: new Date()
+      }
+    })
+    .then(msg => {
+      var trackCode = message.content.replace(`${config.command}`,"").replace(/\s+/g,"");
+      fetch(`https://trackings.post.japanpost.jp/services/srv/search/?requestNo1=${trackCode}&search=%E8%BF%BD%E8%B7%A1%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%88`)
+      .then(res => res.text())
+      .then(body => {
+        var $ = cheerio.load(body);
+        if ($(".txt_l > font:nth-child(1)").text() == "") {
+          var japanpostSet = [];
+          var japanpostInfo = [];
+          var japanpostDate = [];
+          var japanPostPlaceName = [];
+          $("table.tableType01:nth-child(5) > tbody > tr > td:nth-child(2)").each((i, elem) => {japanpostInfo[i] = $(elem).text()});
+          $("table.tableType01:nth-child(5) > tbody > tr > td.w_120:nth-child(1)").each((i, elem) => {japanpostDate[i] = $(elem).text()});
+          if ($(".ttl_line").text().indexOf("ゆうパック") > -1) {
+            var japanpostAuthor = "日本郵便";
+            var japanpostType = $("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text();
+            for (let i = 0; i < japanpostInfo.length; i++) japanpostSet.push({name: `${japanpostDate[i].slice(5)}`, value: `${japanpostInfo[i]}`});
+          }
+          else if ($(".ttl_line").text().indexOf("国際") > -1) {
+            var japanpostAuthor = "日本郵便 / 国際郵便";
+            var japanpostType = `${$("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text()}\n(海外での履歴は現地時間)`;
+            $("table.tableType01:nth-child(5) > tbody:nth-child(1) > tr > td:nth-child(5)").each((i, elem) => {japanPostPlaceName[i] = $(elem).text()});
+            for (let i = 0; i < japanpostInfo.length; i++) {
+              if (japanPostPlaceName[i].replace(/\s+/g,"").replace(/[A-Z]/g,"") == "") {
+                japanpostSet.push({name: `${japanpostDate[i].slice(5)} (${japanPostPlaceName[i].replace(/\s+/g,"")})`, value: `${japanpostInfo[i]}`});
+              }
+              else japanpostSet.push({name: `${japanpostDate[i].slice(5)} (JAPAN)`, value: `${japanpostInfo[i]}`});
+            };
+          }
+          else {
+            var japanpostAuthor = "日本郵便";
+            var japanpostType = $("td.w_480").text();
+            for (let i = 0; i < japanpostInfo.length; i++) japanpostSet.push({name: `${japanpostDate[i].slice(5)}`, value: `${japanpostInfo[i]}`});
+          }
+          msg.edit({
+            embed: {
+              author: {
+                name: japanpostAuthor
+              },
+              color: `0x292929`,
+              title: `荷物の状況: ${japanpostInfo[japanpostInfo.length-1]}`,
+              description: `荷物種別: ${japanpostType}`,
+              fields: japanpostSet,
+              timestamp: new Date()
             }
-            else {
-              japanpostSet.push({name: `${japanpostDate[i].slice(5)} (JAPAN)`, value: `${japanpostInfo[i]}`});
-            }
-          };
+          });
         }
         else {
-          japanpostType = $("td.w_480").text();
-          for (let i = 0; i < japanpostInfo.length; i++) japanpostSet.push({name: `${japanpostDate[i].slice(5)}`, value: `${japanpostInfo[i]}`});
+          msg.edit({
+            embed: {
+              color: `0xff0000`,
+              title: `追跡番号に誤りがあります`,
+              timestamp: new Date()
+            }
+          });
         };
-        message.channel.send({
-          embed: {
-            author: {
-              name: japanpostAuthor
-            },
-            color: `0x292929`,
-            title: `荷物の状況: ${japanpostInfo[japanpostInfo.length-1]}`,
-            description: `荷物種別: ${japanpostType}`,
-            fields: japanpostSet,
-            timestamp: new Date(),
-          }
-        });
-      }
-      else {
-        message.channel.send({
-          embed: {
-            color: `0xff0000`,
-            title: `追跡番号に誤りが有ります`,
-            timestamp: new Date(),
-          }
-        });
-      };
+      });
     });
   };
 });
--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
+
+  // botを使用する際のコマンド (コマンドの後に追跡番号を入力する形になります)
+  // "command": "track" の場合: track 123456789012
   "command": "",
+
+  // Discord Developer Portal (https://discord.com/developers/applications/) で発行したbotのトークン
+  // トークンを漏洩させると乗っ取りの原因になる為管理には注意してください
   "token": ""
+
 };
-
-/*
-
-  command: botを使用する際のコマンド (コマンドの後に追跡番号を入力する形になります)
-  token: DiscordDeveloperPortalで発行したbotのトークン
-  
-*/

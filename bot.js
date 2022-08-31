9c6dd250cb42db42c9a92aa5d42e621c65f80e16
Siwo951 <56748372+Siwo951@users.noreply.github.com>
2022-09-01 05:35:01 +0900

    細かな修正

--- bot.js
+++ bot.js
@@ -23,6 +23,7 @@ client.on("ready", () => {
 
 client.on("messageCreate", message => {
   if (message.author.bot || !message.content.startsWith(config.command)) return;
+
   message.reply({
     embeds: [{
       author: {
@@ -35,11 +36,13 @@ client.on("messageCreate", message => {
   }).then(msg => {
     let body = "";
     const trackCode = message.content.replace(`${config.command}`, "").replace(/\s+/g,"");
+
     https.request(`https://trackings.post.japanpost.jp/services/srv/search/?requestNo1=${trackCode}&search=%E8%BF%BD%E8%B7%A1%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%88`, (res) => {
       res.setEncoding("utf-8");
       res.on("data", (data) => body += data);
       res.on("end", () => {
-        var $ = cheerio.load(body);
+        const $ = cheerio.load(body);
+
         if ($(".txt_l > font:nth-child(1)").text() != "") return msg.edit({
           embeds: [{
             color: "0xff0000",
@@ -47,37 +50,41 @@ client.on("messageCreate", message => {
             timestamp: new Date()
           }]
         });
-        var japanpostDate = [];
-        var japanpostInfo = [];
-        var japanPostPlaceName = [];
-        var japanpostSet = [];
-        $("table.tableType01:nth-child(5) > tbody > tr > td:nth-child(2)").each((i, elem) => {japanpostInfo[i] = $(elem).text()});
-        $("table.tableType01:nth-child(5) > tbody > tr > td.w_120:nth-child(1)").each((i, elem) => {japanpostDate[i] = $(elem).text()});
+
+        const japanPostDate = [];
+        const japanPostInfo = [];
+        const japanPostPlaceName = [];
+        const japanPostSet = [];
+        let japanPostAuthor = "日本郵便";
+        let japanPostType = $("td.w_480").text();
+
+        $("table.tableType01:nth-child(5) > tbody > tr > td:nth-child(2)").each((i, elem) => {japanPostInfo[i] = $(elem).text()});
+        $("table.tableType01:nth-child(5) > tbody > tr > td.w_120:nth-child(1)").each((i, elem) => {japanPostDate[i] = $(elem).text()});
+
         if ($(".ttl_line").text().indexOf("ゆうパック") != -1) {
-          var japanpostType = $("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text();
-          for (let i = 0; i < japanpostInfo.length; i++) japanpostSet.push({name: `${japanpostDate[i].slice(5)}`, value: `${japanpostInfo[i]}`});
+          japanPostType = $("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text();
+          for (let i = 0; i < japanPostInfo.length; i++) japanPostSet.push({name: `${japanPostDate[i].slice(5)}`, value: `${japanPostInfo[i]}`});
         } else if ($(".ttl_line").text().indexOf("国際") != -1) {
-          var japanpostAuthor = "日本郵便 / 国際郵便";
-          var japanpostType = `${$("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text()}\n(海外での履歴は現地時間)`;
+          japanPostAuthor = "日本郵便 / 国際郵便";
+          japanPostType = `${$("table.tableType01:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)").text()}\n(海外での履歴は現地時間)`;
           $("table.tableType01:nth-child(5) > tbody:nth-child(1) > tr > td:nth-child(5)").each((i, elem) => {japanPostPlaceName[i] = $(elem).text()});
-          for (let i = 0; i < japanpostInfo.length; i++) if (japanPostPlaceName[i].replace(/\s+/g,"").replace(/[A-Z]/g,"") == "") {
-            japanpostSet.push({name: `${japanpostDate[i].slice(5)} (${japanPostPlaceName[i].replace(/\s+/g,"")})`, value: `${japanpostInfo[i]}`});
-          } else {
-            japanpostSet.push({name: `${japanpostDate[i].slice(5)} (JAPAN)`, value: `${japanpostInfo[i]}`});
-          }
+          for (let i = 0; i < japanPostInfo.length; i++) japanPostSet.push({
+            name: `${japanPostDate[i].slice(5)} (${japanPostPlaceName[i].replace(/\s+/g,"").replace(/[A-Z]/g,"") == "" ? japanPostPlaceName[i].replace(/\s+/g,"") : "JAPAN"})`,
+            value: `${japanPostInfo[i]}`
+          });
         } else {
-          var japanpostType = $("td.w_480").text();
-          for (let i = 0; i < japanpostInfo.length; i++) japanpostSet.push({name: `${japanpostDate[i].slice(5)}`, value: `${japanpostInfo[i]}`});
+          for (let i = 0; i < japanPostInfo.length; i++) japanPostSet.push({name: `${japanPostDate[i].slice(5)}`, value: `${japanPostInfo[i]}`});
         }
+
         msg.edit({
           embeds: [{
             author: {
-              name: japanpostAuthor ?? "日本郵便"
+              name: japanPostAuthor
             },
             color: "0x292929",
-            title: `荷物の状況: ${japanpostInfo[japanpostInfo.length - 1]}`,
-            description: `荷物種別: ${japanpostType}`,
-            fields: japanpostSet,
+            title: `荷物の状況: ${japanPostInfo[japanPostInfo.length - 1]}`,
+            description: `荷物種別: ${japanPostType}`,
+            fields: japanPostSet,
             timestamp: new Date()
           }]
         });
